e67f3f59f5fb7e42871967980316d23c32e71cd4
Bart <kmiecikjob@gmail.com>
2023-11-20 20:30:42 +0100

    Playing around, trying to remind everything

--- CNN.py
+++ CNN.py
@@ -2,43 +2,28 @@ import torch
 from torch import nn
 import torchvision
 import torchvision.datasets
+from torchvision import transforms
 import os
-import random
-# os.remove("demofile.txt")
+import torch.utils.data as data
+from PIL import Image
 
-# print(torch.__version__, torchvision.__version__)
-# device = 'cuda' if torch.cuda.is_available else 'cpu'
-#
+device = 'cuda' if torch.cuda.is_available else 'cpu'
 # print(device)
-#
-# root = torchvision.datasets.ImageFolder(root='Dataset')
-#
-# print(root)
+BATCH_SIZE = 64
+EPOCHS = 2
+LEARNING_RATE = 0.0001
+data_transform = transforms.Compose([
+    transforms.ToTensor()
+])
+def run():
+    train_data = torchvision.datasets.ImageFolder(root='Dataset', transform=data_transform)
+    train_data_loader = data.DataLoader(train_data, BATCH_SIZE, True, num_workers=2)
+    train_features_batch, train_labels_batch = next(iter(train_data_loader))
+    class_names = train_data.classes
+    class_dict = train_data.class_to_idx
+    print(train_features_batch.shape)
+    print(train_labels_batch.shape)
 
-# files = os.listdir('Dataset/NoChange')
-# to_delete = []
-
-# def get_rnd(max):
-#     rnd = random.randint(1, max)
-#     return rnd
-#
-# for i in range(len(files)-4993):
-#     while True:
-#         rnd = get_rnd(len(files))
-#         if to_delete.__contains__(rnd):
-#             pass
-#         else:
-#             to_delete.append(rnd)
-#             break
-#
-#
-# to_delete.sort(reverse=True)
-# print(f'{len(to_delete)} and {len(files)-4993}')
-# print(to_delete)
-
-
-# for x in to_delete:
-#     if os.path.exists(f"Dataset/NoChange/{files[x-1]}"):
-#         os.remove(f"Dataset/NoChange/{files[x-1]}")
-#     else:
-#         print("The file does not exist")
+# print(f'Batch: {train_features_batch} Label: {train_labels_batch}')
+if __name__ == '__main__':
+    run()
